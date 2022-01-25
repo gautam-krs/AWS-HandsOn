ccc3524e13456df2c6848e7d7c7fe3182f61b159
Your Name <you@example.com>
2022-01-25 17:39:22 +0530

    Modified Account_Cost.sh

--- AWS_CLI_Projects/Account_Cost.sh
+++ AWS_CLI_Projects/Account_Cost.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+#List All the Accouns within the organization
 Acct_Name=$(aws organizations list-accounts --query Accounts[*].Id --output text)
 array=$Acct_Name
 for i in ${array[@]}
 do
-	echo "Account Name: "$i
-    aws ce get-cost-and-usage --profile $i \
+    #Generates the blended cost for all the accounts in loop
+    echo "Cost for Account : " $i
+	aws ce get-cost-and-usage --profile $i \
     --time-period Start=2022-01-01,End=2022-02-01 \
     --granularity MONTHLY \
-    --metrics "BlendedCost" 
+    --metrics "BlendedCost" \
+    --query ResultsByTime[*].Total.BlendedCost
+
+
 done 
